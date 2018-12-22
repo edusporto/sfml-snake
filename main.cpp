66224da5aea832d06e91f8745d23532408a06cac
Eduardo Sandalo Porto <30930225+edusporto@users.noreply.github.com>
2018-12-21 23:21:43 -0200

    Organizing code

--- main.cpp
+++ main.cpp
@@ -5,32 +5,37 @@
 #include "Snake.h"
 #include "Food.h"
 
-// Variables used in the program
+/* ========== Variables used in the program ========== */
+// Window configuration
+float fps = 30.f;
 int windowWidth = 1280;
 int windowHeight = 720;
+
+// Snake configuration
 float speed = 10.f;
 float size  = 10.f;
 int growth  = 20;
 float startingX = 100.f;
 float startingY = 100.f;
 sf::Color color = sf::Color::Green;
-float fps = 30.f;
 
+Snake snake(windowWidth, windowHeight, size, speed, growth, color, startingX, startingY);
+Food food(windowWidth, windowHeight, size, sf::Color::White);
+Snake::Direction nextMove = Snake::Right;
+Snake::Direction lastMove = Snake::Right;
+
+// Point configuration
 unsigned int points = 0;
 
+// States that the game can be in
 enum GameState {
     Start, Playing, Paused, Over
 };
 GameState state = Start;
 
-Snake snake(windowWidth, windowHeight, size, speed, growth, color, startingX, startingY);
-Food food(windowWidth, windowHeight, size, sf::Color::White);
-
-Snake::Direction nextMove = Snake::Right;
-Snake::Direction lastMove = Snake::Right;
 
 
-// Helper functions
+/* ========== Helper functions ========== */
 
 void restart()
 {
@@ -60,17 +65,27 @@ void gameLoop()
     }
 }
 
+// Function executed whenever the player presses the space bar
+void spaceBarPressed()
+{
+    switch (state)
+    {
+        case Start:   state = Playing; break;
+        case Playing: state = Paused;  break;
+        case Paused:  state = Playing; break;
+        case Over:    state = Start; restart(); break;
+    }
+}
 
 
 int main()
 {
+    /* ========== Configuring UI elements ========== */
+
+    // Configuring the window
     sf::Uint32 style = sf::Style::Titlebar | sf::Style::Close | sf::Style::Resize;
     sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Snake", style);
     window.setFramerateLimit(fps);
-
-    //Snake snake(size, speed, growth, color, startingX, startingY);
-
-    //Food food(windowWidth, windowHeight, size, sf::Color::White);
 
     // Setting different texts showed on the screen
     sf::Font font;
@@ -78,21 +93,19 @@ int main()
         std::cout << "Could not load resources/fonts/arial.ttf" << std::endl;
         return 0;
     }
-    // Paused text
-    sf::Text pausedText;
-    pausedText.setFont(font);
-    pausedText.setString("Paused. Press [SPACE] to unpause");
-    pausedText.setFillColor(sf::Color::Blue);
-    pausedText.setCharacterSize(30);
     // Text shown at the start of the game
     sf::Text startText("Press [SPACE] to start", font, 30);
     startText.setFillColor(sf::Color::Yellow);
+    // Paused text
+    sf::Text pausedText("Paused. Press [SPACE] to unpause", font, 30);
+    pausedText.setFillColor(sf::Color::Blue);
     // Text shown in game over
     sf::Text gameOverText("Game Over. Press [SPACE] to restart", font, 30);
     gameOverText.setFillColor(sf::Color::Red);
 
     while (window.isOpen())
     {
+        // Shows the current amount of points in the title bar
         window.setTitle("Snake. Points: " + std::to_string(points));
 
         sf::Event e;
@@ -112,46 +125,31 @@ int main()
                     if (e.text.unicode == 'f')
                         food = Food(windowWidth, windowHeight, size, sf::Color::White);
                     if (e.text.unicode == ' ') {
-                        switch (state)
-                        {
-                            case Start:   state = Playing; break;
-                            case Playing: state = Paused;  break;
-                            case Paused:  state = Playing; break;
-                            case Over:    state = Start; restart(); break;
-                        }
+                        spaceBarPressed();
                     }
                     break;
             }
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up)) {
-            //snake.move(0);
             if (lastMove != Snake::Down)
                 nextMove = Snake::Up;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
-            //snake.move(1);
             if (lastMove != Snake::Right)
                 nextMove = Snake::Left;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down)) {
-            //snake.move(2);
             if (lastMove != Snake::Up)
                 nextMove = Snake::Down;
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
-            //snake.move(3);
             if (lastMove != Snake::Left)
                 nextMove = Snake::Right;
         }
-
-        /*if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
-            sf::Vector2i pos = sf::Mouse::getPosition(window);
-            player.setPosition((float)pos.x, (float)pos.y);
-        }*/
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Escape)) {
             window.close();
@@ -174,8 +172,8 @@ int main()
 
         // Drawing objects on the screen
 		sf::RectangleShape previous;
-        for (sf::RectangleShape piece : snake.getBody()) {
-        	// Preventes pieces of the snake that are in the same position be drawn
+        for (sf::RectangleShape& piece : snake.getBody()) {
+        	// Prevents pieces of the snake that are in the same position be drawn
         	// Helps in performance of really big snakes
             if (previous.getPosition() == piece.getPosition())
            	    break;
